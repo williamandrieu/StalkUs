e220ced04706786dd0a95b8f7d6a0e5aad4743f5
William <wulloase95@gmail.com>
2019-04-08 16:59:36 +0200

    william bdd

--- index.php
+++ index.php
@@ -4,10 +4,25 @@ echo '
 	<head>
 	  <title>StalkUS</title> 
 	</head>
+	<link href="./CSS/message.css" rel="stylesheet">
 <body>';
 
 
-require('./PHP/TopBar.php');
+require('./PHP/Message.php');
+require('./PHP/BDD.php');
+
+$bdd = new BDD();
+
+$resultUser = $bdd->getUserId(1);
+foreach ($resultUser as $val) {
+	$resultMessage = $bdd->getMessageExpediteur($val[0]);
+	foreach ($resultMessage as $value) {
+		$message = new Message($value[3],$value[2],$value[1]);
+		echo $message->printMessage($val[2]);
+}
+}
+
+
 
 echo "</body>
 </html>";
