17f406f2aba00bf9b8345793b9e05274ffd39f27
William <wulloase95@gmail.com>
2019-04-08 14:03:43 +0200

    new topbar

---
+++ PHP/TopBar.php
@@ -0,0 +1,12 @@
+<?php
+
+$text = '
+  <div>
+    <div><img src/></div>
+    <p>Ceci est généré par le php</p>
+  </div>';
+
+
+echo $text;
+
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+echo '
+<html>
+	<head>
+	  <title>StalkUS</title> 
+	</head>
+<body>';
+
+
+require('./PHP/TopBar.php');
+
+echo "</body>
+</html>";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
